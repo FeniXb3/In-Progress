0fc8f9571f99db0d3701581a4dc9ea23a5f3ba61
FeniXb3 <fenix.b3@gmail.com>
2016-03-02 02:22:58 +0100

    Added component to move to point

---
+++ Assets/Scripts/FungusExtensions/ClickablePrecise.cs
@@ -0,0 +1,31 @@
+﻿using Fungus;
+using UnityEngine;
+
+public class ClickablePrecise : Clickable2D
+{
+	private Vector3 lastClickPoint = Vector3.zero;
+
+	protected override void OnMouseDown()
+	{
+		SetLastlickPoint();
+		base.OnMouseDown();
+	}
+
+	private void SetLastlickPoint()
+	{
+		if (!clickEnabled)
+		{
+			return;
+		}
+
+		var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		Physics.Raycast(ray, out hit);
+		lastClickPoint = hit.point;
+	}
+
+	public Vector3 GetLastClickPoint()
+	{
+		return lastClickPoint;
+	}
+}
